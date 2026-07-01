e3112230ef2dfd7ac9f3d94149289fee30d5f6b6
Jeisson Espitia <json9346@gmail.com>
2026-07-01 02:23:08 -0500

    Format Terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -129,7 +129,7 @@ resource "aws_instance" "servidor" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
   key_name               = var.key_name
-  iam_instance_profile = aws_iam_instance_profile.profile.name
+  iam_instance_profile   = aws_iam_instance_profile.profile.name
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.sg.id]
 
@@ -170,9 +170,9 @@ resource "aws_db_instance" "mysql" {
   engine_version         = "8.0"
   instance_class         = "db.t3.micro"
   allocated_storage      = 20
-  db_name  = var.db_name
-  username = var.db_user
-  password = var.db_password
+  db_name                = var.db_name
+  username               = var.db_user
+  password               = var.db_password
   db_subnet_group_name   = aws_db_subnet_group.db_subnets.name
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
   skip_final_snapshot    = true
